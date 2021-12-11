5c1a7f305991478240a942efe4df719042e63bc0
hoangduong-coder <hoangduongphantri@gmail.com>
2021-12-11 23:34:18 +0200

    Schema

---
+++ social-media-restapi/models/Post.js
@@ -0,0 +1,44 @@
+const mongoose = require ('mongoose');
+
+const PostSchema = new mongoose.Schema (
+  {
+      userId: {
+          type: String,
+          required: true,
+      },
+      status: {
+          type: String,
+          max: 1000,
+      },
+      img: {
+          type: String
+      },
+      like: {
+          type: Array,
+          default: [],
+      },
+      dislike: {
+          type: Array,
+          default: [],
+      },
+      love: {
+          type: Array,
+          default: [],
+      },
+      funny: {
+          type: Array,
+          default: [],
+      },
+      disappointed: {
+          type: Array,
+          default: [],
+      },
+      angry: {
+          type: Array,
+          default: [],
+      },
+  },
+  {timestamps: true}
+);
+
+module.exports = mongoose.model ('Post', PostSchema);
---
+++ social-media-restapi/models/User.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema (
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 30,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+        type: String,
+        max: 100,
+    },
+    hometown: {
+        type: String,
+        max: 50,
+    },
+    reside: {
+        type: String,
+        max: 50,
+    },
+    gender: {
+        type: Number,
+        enum: [1,2,3,4]
+    },
+    relationship: {
+        type: Number,
+        enum: [1,2,3,4]
+    }
+  },
+  {timestamps: true}
+);
+
+module.exports = mongoose.model('User', UserSchema);
